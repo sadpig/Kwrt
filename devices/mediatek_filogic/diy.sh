3807368c699921c1400e5c6e4b0f98a6f25cbd73
Tan Wei <gztanwei@gmail.com>
2026-09-14 22:20:01 +0800

    brand: Twrt rename, LAN 10.10.8.1, add quickstart homepage/wizard (fork-local)

--- devices/mediatek_filogic/diy.sh
+++ devices/mediatek_filogic/diy.sh
@@ -6,3 +6,20 @@ SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 #bash $SHELL_FOLDER/../common/kernel_6.6.sh
 
 sed -i -E -e 's/ ?root=\/dev\/fit0 rootwait//' -e "/rootdisk =/d" -e '/bootargs.* = ""/d' target/linux/mediatek/dts/*{qihoo-360t7,netcore-n60*,h3c-magic-nx30-pro,jdcloud-re-cp-03,cmcc-rax3000m,jcg-q30-pro,tplink-tl-xdr*,abt-asr3000,komi-a31,nokia-ea0326gmp,bt-r320}*.dts*
+
+# ---- fork-local: Twrt branding (runs after common diy.sh which did OpenWrt->Kwrt) ----
+# dist name / hostname / default Wi-Fi SSID / image prefix
+sed -i "s/Kwrt/Twrt/g" package/base-files/files/bin/config_generate \
+	package/base-files/image-config.in \
+	package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc \
+	config/Config-images.in Config.in include/u-boot.mk include/version.mk || true
+
+# default LAN IP: 10.0.0.1 -> 10.10.8.1 (common diy.sh set 192.168.1 -> 10.0.0)
+sed -i "s/10\.0\.0\./10.10.8./g" package/base-files/files/bin/config_generate
+
+# rebrand kiddin9 my-default-settings (hostname fallback, dhcp domain, wizard/nginx shortcuts, IPs)
+for f in feeds/kiddin9/my-default-settings/files/etc/uci-defaults/99-default-settings \
+	 feeds/kiddin9/my-default-settings/files/etc/config/base_config; do
+	[ -f "$f" ] && sed -i -e "s/Kwrt/Twrt/g" -e "s/kwrt/twrt/g" -e "s/10\.0\.0\./10.10.8./g" "$f" || true
+done
+exit 0
